ad6f7b97b50c9120a4dce0e67262a82e567e65f4
Oskar Liew <oskar@liew.se>
2025-02-19 21:06:50 +0100

    More WIP

--- src/hnsw_paper.py
+++ src/hnsw_paper.py
@@ -3,14 +3,17 @@ from collections import defaultdict
 from heapq import heappop, heappush
 import math
 import random
+import time
 
 from common import cosine_similarity, random_vector
+
+DIM = 32
 
 
 class HNSW:
     def __init__(self, m_max: int, ef_construction: int, n_layers: int) -> None:
         self.m_max = m_max
-        self.m_max_0 = m_max
+        self.m_max_0 = m_max * 2
         self.ef_construction = ef_construction
         self.n_layers = n_layers
 
@@ -44,16 +47,16 @@ class HNSW:
         insert_layer = min(insert_layer, self.n_layers - 1)
 
         # Find start point in insert layer
-        start_idx = 0
+        start_indices = [0]
         for i_layer in range(self.n_layers - 1, insert_layer, -1):
-            neighbours = self._search_layer(start_idx, vector, i_layer, ef=1)
-            start_idx = neighbours[0][1]
+            neighbours = self._search_layer(start_indices, vector, i_layer, ef=1)
+            start_indices = [neighbours[0][1]]
 
         # Go through all layers from insert layer and down
         for i_layer in range(insert_layer, -1, -1):
             # Find the nearest neighbours of the vector
             neighbours = self._search_layer(
-                start_idx, vector, i_layer, ef=self.ef_construction
+                start_indices, vector, i_layer, ef=self.ef_construction
             )
 
             # Set neighbours. Use m_max_0 if bottom layer
@@ -66,16 +69,16 @@ class HNSW:
                 insort(self.neighbours[i_layer][n_id], (n_sim, insert_idx))
                 self.neighbours[i_layer][n_id] = self.neighbours[i_layer][n_id][:m_max]
 
-            start_idx = neighbours[0][1]  # OR maybe all?
+            start_indices = [n[1] for n in neighbours]
 
     def _search_layer(
-        self, start_idx: int, vector: list[float], layer: int, ef: int
+        self, start_indices: list[int], vector: list[float], layer: int, ef: int
     ) -> list[tuple[float, int]]:
-        sim = cosine_similarity(vector, self.vectors[start_idx])
-
-        visited = {start_idx}
-        candidates = [(sim, start_idx)]
-        neighbours = [(sim, start_idx)]
+        visited = set(start_indices)
+        candidates = [
+            (cosine_similarity(vector, self.vectors[i]), i) for i in start_indices
+        ]
+        neighbours = candidates.copy()
 
         while candidates:
             candidate = heappop(candidates)
@@ -85,13 +88,13 @@ class HNSW:
                 break
 
             for c_neigh in self.neighbours[layer][candidate[1]]:
-                if c_neigh in visited:
+                if c_neigh[1] in visited:
                     continue
 
                 visited.add(c_neigh[1])
                 furthest_neigh = neighbours[-1]
                 neigh_sim = cosine_similarity(vector, self.vectors[c_neigh[1]])
-                if neigh_sim < neighbours[0][0]:
+                if neigh_sim < neighbours[0][0] or len(neighbours) < ef:
                     heappush(candidates, (neigh_sim, c_neigh[1]))
                     insort(neighbours, (neigh_sim, c_neigh[1]))
                     if len(neighbours) > ef:
@@ -100,18 +103,88 @@ class HNSW:
         return neighbours
 
     def search(self, vector: list[float], ef: int) -> list[tuple[float, int]]:
-        start_idx = 0
+        start_indices = [0]
         neighbours = []
         for i_layer in range(self.n_layers - 1, -1, -1):
-            neighbours = self._search_layer(start_idx, vector, i_layer, ef)
-            start_idx = neighbours[0][-1]
+            neighbours = self._search_layer(start_indices, vector, i_layer, ef)
+            start_indices = [neighbours[0][1]]
         return neighbours
+
+    def _select_neighbours_heuristic(
+        self,
+        layer: int,
+        candidates: list[tuple[float, int]],
+        m: int,
+        extend: bool = False,
+        keep: bool = False,
+    ) -> list[tuple[float, int]]:
+        out = []
+        neighbours = candidates.copy()
+
+        # Extend search to include neighbours of neighbours
+        if extend:
+            for candidate in candidates:
+                for candidate_adj in self.neighbours[layer][candidate[1]]:
+                    if candidate_adj in neighbours:
+                        continue
+                    heappush(neighbours, candidate_adj)
+
+        # Add all neighbours that are sufficiently close
+        discarded = []
+        while neighbours and len(out) < m:
+            candidate = heappop(candidates)
+            if not out or candidate[0] < out[0]:
+                heappush(out, candidate)
+            else:
+                heappush(discarded, candidate)
+
+        # Keep pruned connections until #connections is m
+        if keep:
+            while discarded and len(out) < m:
+                nearest_discarded = heappop(discarded)
+                heappush(out, nearest_discarded)
+
+            raise NotImplemented
+
+        return out
+
+
+def main():
+    print("Starting")
+    vectors = [random_vector(DIM) for _ in range(1000)]
+
+    print("Indexing")
+    t0 = time.time()
+    # Index
+    index = HNSW(n_layers=4, m_max=5, ef_construction=32)
+    for v in vectors:
+        index.insert(v)
+
+    index_time = time.time() - t0
+    print(f"Indexing took: {index_time:.5f} seconds")
+
+    for l, e in index.neighbours.items():
+        print(l, len(e))
+
+    # Search
+    search_vector = random_vector(DIM)
+
+    print("Searching")
+
+    # NSW
+    t0 = time.time()
+    hnsw_sims = index.search(search_vector, ef=16)
+    hnsw_time = time.time() - t0
+
+    print("HNSW", hnsw_sims[0][0], hnsw_time)
+
+    # Naive
+    t0 = time.time()
+    naive_best = min(cosine_similarity(search_vector, v) for v in vectors)
+    naive_time = time.time() - t0
+
+    print("Naive", naive_best, naive_time)
 
 
 if __name__ == "__main__":
-    index = HNSW(3, 8, 3)
-    for v in (random_vector(8) for _ in range(100)):
-        index.insert(v)
-
-    # __import__("pprint").pprint(index.neighbours)
-    print(index.search(random_vector(8), 8))
+    main()
