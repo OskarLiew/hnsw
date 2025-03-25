b06b0d25982a2de81d49995fe1799bdccbf13a9f
Oskar Liew <oskar@liew.se>
2025-03-25 20:51:57 +0100

    Updated algorithm

--- src/common.py
+++ src/common.py
@@ -6,12 +6,12 @@ def random_vector(size: int) -> list[float]:
     return [random.random() for _ in range(size)]
 
 
-def cosine_similarity(v1: list[float], v2: list[float]) -> float:
+def cosine_distance(v1: list[float], v2: list[float]) -> float:
     assert len(v1) == len(v2)
 
     m1 = vector_norm(v1)
     m2 = vector_norm(v2)
-    return sum(e1 * e2 / (m1 * m2) for e1, e2 in zip(v1, v2))
+    return -sum(e1 * e2 / (m1 * m2) for e1, e2 in zip(v1, v2))
 
 
 def vector_norm(v: list[float]) -> float:
